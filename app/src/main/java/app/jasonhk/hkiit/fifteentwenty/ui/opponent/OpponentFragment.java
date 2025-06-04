ae40336730c33852244743d94574b73225ce6b4b
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2025-06-04 18:57:33 +0800

    Added opponent select

--- app/src/main/java/app/jasonhk/hkiit/fifteentwenty/ui/opponent/OpponentFragment.java
+++ app/src/main/java/app/jasonhk/hkiit/fifteentwenty/ui/opponent/OpponentFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,6 +21,8 @@ import app.jasonhk.hkiit.fifteentwenty.R;
 
 public class OpponentFragment extends Fragment
 {
+    private String[] opponents;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
@@ -30,8 +35,31 @@ public class OpponentFragment extends Fragment
     {
         super.onViewCreated(view, savedInstanceState);
 
-        var navigation = NavHostFragment.findNavController(this);
+        opponents = getResources().getStringArray(R.array.opponents);
 
-        navigation.navigate(OpponentFragmentDirections.actionFragmentOpponentToFragmentGameChoices("Name"));
+        ListView opponentsList = view.findViewById(R.id.fragment_opponent_list_opponents);
+        opponentsList.setAdapter(new ArrayAdapter<>(requireContext(), R.layout.adapter_opponent, opponents));
+        opponentsList.setOnItemClickListener(this::onOpponentsListItemClick);
+
+        ViewCompat.setOnApplyWindowInsetsListener(view.findViewById(R.id.fragment_opponent_toolbar_wrapper), (v, windowInsets) ->
+        {
+            var insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout());
+            v.setPadding(insets.left, 0, insets.right, 0);
+            ((ViewGroup.MarginLayoutParams) v.getLayoutParams()).topMargin = insets.top; // Fix the stupid edge-to-edge bug with CollapsingToolbarLayout.
+            return WindowInsetsCompat.CONSUMED;
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(opponentsList, (v, windowInsets) ->
+        {
+            var insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout());
+            v.setPadding(insets.left, 0, insets.right, insets.bottom + insets.top);
+            return windowInsets;
+        });
+    }
+
+    private void onOpponentsListItemClick(AdapterView<?> parent, View view, int position, long id)
+    {
+        NavHostFragment.findNavController(this).navigate(OpponentFragmentDirections
+                .actionFragmentOpponentToFragmentGameChoices(opponents[position]));
     }
 }
