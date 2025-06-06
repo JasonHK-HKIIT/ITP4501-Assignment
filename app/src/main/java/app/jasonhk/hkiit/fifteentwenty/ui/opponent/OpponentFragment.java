7b181266b0517f27a9c5ced331b26330d9b127e5
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2025-06-06 23:16:35 +0800

    - Added landscape for `OpponentFragment` - Updated edge-to-edge layout

--- app/src/main/java/app/jasonhk/hkiit/fifteentwenty/ui/opponent/OpponentFragment.java
+++ app/src/main/java/app/jasonhk/hkiit/fifteentwenty/ui/opponent/OpponentFragment.java
@@ -1,5 +1,6 @@
 package app.jasonhk.hkiit.fifteentwenty.ui.opponent;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,15 +11,19 @@ import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
 import app.jasonhk.hkiit.fifteentwenty.R;
 
 public class OpponentFragment extends Fragment
 {
+    private NavController navigation;
+
     /** List of opponents. */
     private String[] opponents;
 
@@ -34,8 +39,13 @@ public class OpponentFragment extends Fragment
     {
         super.onViewCreated(view, savedInstanceState);
 
+        navigation = NavHostFragment.findNavController(this);
+
         // Retrieve opponents list
         opponents = getResources().getStringArray(R.array.opponents);
+
+        Toolbar toolbar = view.findViewById(R.id.fragment_opponent_toolbar);
+        toolbar.setNavigationOnClickListener((v) -> requireActivity().getOnBackPressedDispatcher().onBackPressed());
 
         ListView opponentsList = view.findViewById(R.id.fragment_opponent_list_opponents);
         opponentsList.setAdapter(new ArrayAdapter<>(requireContext(), R.layout.adapter_opponent, opponents));
@@ -45,21 +55,20 @@ public class OpponentFragment extends Fragment
         ViewCompat.setOnApplyWindowInsetsListener(view.findViewById(R.id.fragment_opponent_toolbar_wrapper), (v, windowInsets) ->
         {
             var insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout());
-            v.setPadding(insets.left, 0, insets.right, 0);
-            ((ViewGroup.MarginLayoutParams) v.getLayoutParams()).topMargin = insets.top; // Fix the stupid edge-to-edge bug with CollapsingToolbarLayout.
+            v.setPadding(insets.left, insets.top, insets.right, 0);
             return windowInsets;
         });
         ViewCompat.setOnApplyWindowInsetsListener(opponentsList, (v, windowInsets) ->
         {
             var insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout());
-            v.setPadding(insets.left, 0, insets.right, insets.bottom + insets.top);
+            v.setPadding(insets.left, 0, insets.right, insets.bottom);
             return windowInsets;
         });
     }
 
     private void onOpponentsListItemClick(AdapterView<?> parent, View view, int position, long id)
     {
-        NavHostFragment.findNavController(this).navigate(OpponentFragmentDirections
-                .actionFragmentOpponentToFragmentGameChoices(opponents[position]));
+        navigation.navigate(OpponentFragmentDirections.actionFragmentOpponentToFragmentGameChoices(
+                opponents[position]));
     }
 }
