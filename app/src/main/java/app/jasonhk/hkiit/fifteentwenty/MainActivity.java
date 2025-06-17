d19ace1f91d90fe358cd27a5952724c29bd0a5a0
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2025-06-17 14:40:58 +0800

    Added dynamic colour support

--- app/src/main/java/app/jasonhk/hkiit/fifteentwenty/MainActivity.java
+++ app/src/main/java/app/jasonhk/hkiit/fifteentwenty/MainActivity.java
@@ -1,12 +1,20 @@
 package app.jasonhk.hkiit.fifteentwenty;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.preference.PreferenceManager;
 
-public class MainActivity extends AppCompatActivity
+import com.google.android.material.color.DynamicColors;
+import com.google.android.material.color.DynamicColorsOptions;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -15,6 +23,37 @@ public class MainActivity extends AppCompatActivity
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        var preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        preferences.registerOnSharedPreferenceChangeListener(this);
+
+        setDynamicColors(preferences.getBoolean("dynamic_color", false));
+
+//        DynamicColors.applyToActivityIfAvailable(this);
+//        DynamicColors.applyToActivityIfAvailable(this, new DynamicColorsOptions.Builder().setThemeOverlay(R.style.Theme_FifteenTwenty).build());
+
         var screen = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.activity_main_fragment_screen);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences preferences, @Nullable String key)
+    {
+        if (Objects.equals(key, "dynamic_color"))
+        {
+            setDynamicColors(preferences.getBoolean("dynamic_color", false));
+            recreate();
+        }
+    }
+
+    private void setDynamicColors(boolean enabled)
+    {
+        if (enabled)
+        {
+            DynamicColors.applyToActivityIfAvailable(this);
+        }
+        else
+        {
+            DynamicColors.applyToActivityIfAvailable(this, new DynamicColorsOptions.Builder()
+                    .setThemeOverlay(R.style.Theme_FifteenTwenty).build());
+        }
     }
 }
